8574bac5078b454465db79c4521ca5dfd13281ab
hazemSaeed <hazemalb96@gmail.com>
2020-04-03 15:59:17 +0300

    create files for react app and added file homepage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import HomePage from './components/homePage.component';
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
