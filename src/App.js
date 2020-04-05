44cffedd1e40de5ce91499802c33706f9e849ccf
hazemSaeed <hazemalb96@gmail.com>
2020-04-05 10:44:00 +0300

    add routing to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homePage.component';
+import './App.css';
+
+const Hats = ({ history }) => {
+  const str = history.location.pathname.split('/').join(' ').toUpperCase();
+  return (
+    <div>
+      <h1>{str}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/shop" component={Hats} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './menu-item.styles.scss';
-const MenuItem = ({ title, imageUrl, size }) => {
+import { withRouter } from 'react-router-dom';
+const MenuItem = ({ title, imageUrl, size, linkUrl, match, history }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
@@ -16,4 +20,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
